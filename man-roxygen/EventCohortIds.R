45262b5c814d715556c79bf25c8ca692f4c38ee4
Gowtham Rao <rao@ohdsi.org>
2022-11-08 09:47:12 -0500

    Initial commit

---
+++ man-roxygen/EventCohortIds.R
@@ -0,0 +1,1 @@
+#' @param eventCohortIds   A vector of one or more Cohort Ids corresponding to event cohort (s).
---
+++ man-roxygen/TargetCohortIds.R
@@ -0,0 +1,1 @@
+#' @param targetCohortIds   A vector of one or more Cohort Ids corresponding to target cohort (s).
