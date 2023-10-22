0f3d68fcb958e511cbb7910c2ca26b27d3b5d364
Martijn Schuemie <schuemie@ohdsi.org>
2023-10-22 22:57:48 +0200

    Simplying dependencies of Shiny app
    Co-authored-by: Gowtham Rao <rao@ohdsi.org>
    Co-authored-by: Admin_mschuemi <Admin_mschuemi@its.jnj.com>

--- inst/shiny/R/private.R
+++ inst/shiny/R/private.R
@@ -30,3 +30,21 @@ readData <- function(databaseId,
     return(NULL)
   }
 }
+
+
+snakeCaseToCamelCase <- function(string) {
+  string <- tolower(string)
+  for (letter in letters) {
+    string <- gsub(paste("_", letter, sep = ""), toupper(letter), string)
+  }
+  string <- gsub("_([0-9])", "\\1", string)
+  return(string)
+}
+
+camelCaseToTitleCase <- function(string) {
+  string <- gsub("([A-Z])", " \\1", string)
+  string <- gsub("([a-z])([0-9])", "\\1 \\2", string)
+  substr(string, 1, 1) <- toupper(substr(string, 1, 1))
+  return(string)
+}
+
